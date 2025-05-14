1912c6840eeef0356c48de8352df4c2f3499f65f
DaianePorcena <daiporcenna@gmail.com>
2025-05-14 00:05:53 -0300

    fet: Cria a tela de feeds

---
+++ agiledevs/lib/components/metodo_card.dart
@@ -0,0 +1,101 @@
+import 'package:agiledevs/Utils/estado.dart';
+import 'package:agiledevs/models/metodo.dart';
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+class MetodoCard extends StatelessWidget {
+  final Metodo metodos;
+  final bool usuarioLogado;
+  const MetodoCard(this.metodos, this.usuarioLogado, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        estadoApp.showDetails(metodos.id);
+      },
+      child: Card(
+        color: const Color(0x50150050),
+        elevation: 5,
+        child: Container(
+          height: 200,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(metodos.image),
+              fit: BoxFit.cover,
+              opacity: 0.42,
+            ),
+            color: const Color(0x80150050),
+            borderRadius: BorderRadius.circular(8)
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            metodos.title.toUpperCase(),
+                            softWrap: true,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          child: IconButton(
+                            icon: const Icon(Icons.bookmark_outline),
+                            color: usuarioLogado ? Colors.white : Colors.grey,
+                            onPressed: () {
+                              usuarioLogado
+                                  ? Toast.show("Método salvo com sucesso", duration: Toast.lengthShort,
+                    gravity: Toast.bottom,)
+                                  : Toast.show("Você precisa estar logado para salvar", duration: Toast.lengthShort,
+                    gravity: Toast.bottom,);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            metodos.description,
+                            softWrap: true,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.star, color: Colors.yellow, size: 25),
+                            // Text(
+                            //   'likes: ${mt.likes}',
+                            // )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ agiledevs/lib/screens/metodos.dart
@@ -0,0 +1,196 @@
+import 'dart:convert';
+
+import 'package:agiledevs/Utils/autenticador.dart';
+import 'package:agiledevs/Utils/estado.dart';
+import 'package:agiledevs/components/metodo_card.dart';
+import 'package:agiledevs/models/metodo.dart';
+import 'package:flat_list/flat_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:toast/toast.dart';
+
+class Metodos extends StatefulWidget {
+  const Metodos({super.key});
+
+  @override
+  State<Metodos> createState() => _MetodosState();
+}
+
+const int tamanhoPagina = 4;
+
+class _MetodosState extends State<Metodos> {
+  dynamic _feedEstatico = [];
+  List<dynamic> _metodos = [];
+
+  int _proximaPagina = 1;
+  bool _carregando = false;
+
+  late TextEditingController _controladorFiltragem;
+  String _filtro = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    ToastContext().init(context);
+    _controladorFiltragem = TextEditingController();
+    _readFeedEstatico();
+  }
+
+  Future<void> _readFeedEstatico() async {
+    final String conteudoJson = await rootBundle.loadString(
+      "lib/data/json/feed.json",
+    );
+    _feedEstatico = await json.decode(conteudoJson);
+    _carregarMetodos();
+  }
+
+  void _carregarMetodos() {
+    if (_carregando || _metodos.length >= _feedEstatico["metodos"].length) {
+      return;
+    }
+
+    setState(() {
+      _carregando = true;
+    });
+
+    var maisMetodos = [];
+    if (_filtro.isNotEmpty) {
+      maisMetodos =
+          _feedEstatico["metodos"].where((item) {
+            String title = item["title"] ?? "";
+            return title.toLowerCase().contains(_filtro.toLowerCase());
+          }).toList();
+    } else {
+      final inicio = (_proximaPagina - 1) * tamanhoPagina;
+      final fim = (_proximaPagina * tamanhoPagina).clamp(
+        0,
+        _feedEstatico["metodos"].length,
+      );
+
+      maisMetodos = _feedEstatico["metodos"].sublist(inicio, fim);
+    }
+
+    setState(() {
+      _metodos.addAll(
+        maisMetodos.map((json) => Metodo.fromJson(json)).toList(),
+      );
+      _proximaPagina++;
+      _carregando = false;
+    });
+  }
+
+  Future<void> _atualizarFeed() async {
+    _metodos = [];
+    _proximaPagina = 1;
+    _carregarMetodos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool usuarioLogado = estadoApp.usuario != null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            const Text("AgileDevs"),
+            SizedBox(width: 6),
+            Image.asset(
+              "lib/data/images/logo.png",
+              fit: BoxFit.cover,
+              height: 30,
+              width: 30,
+            ),
+          ],
+        ),
+        actions: [
+          usuarioLogado
+              ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    estadoApp.onLogout();
+                  });
+                  Toast.show(
+                    "Deslogado com sucesso",
+                    duration: Toast.lengthShort,
+                    gravity: Toast.bottom,
+                  );
+                },
+                icon: const Icon(Icons.logout),
+              )
+              : IconButton(
+                onPressed: () {
+                  Usuario usuario = Usuario(
+                    "daiane porcena",
+                    "daiporcenna@gmail.com",
+                  );
+                  setState(() {
+                    estadoApp.onLogin(usuario);
+                  });
+                  Toast.show(
+                    "Logado com sucesso",
+                    duration: Toast.lengthShort,
+                    gravity: Toast.bottom,
+                  );
+                },
+                icon: const Icon(Icons.login),
+              ),
+        ],
+      ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 17.0, bottom: 8.0, left: 10.0, right: 10.0),
+            child: TextField(
+              controller: _controladorFiltragem,
+              onChanged: (descricao) {
+                setState(() {
+                  _filtro = descricao;
+                });
+                _atualizarFeed();
+              },
+              decoration: const InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFF150050),
+                    width: 1
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFF150050),
+                    width: 2
+                  ),
+                ),
+                hintText: "Pesquisar",
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatList(
+              data: _metodos,
+              loading: _carregando,
+              onRefresh: () {
+                _filtro = "";
+                _controladorFiltragem.clear();
+                return _atualizarFeed();
+              },
+              onEndReached: () => _carregarMetodos(),
+              buildItem: (item, int indice) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 10.0,
+                  ),
+                  child: Container(child: MetodoCard(item, usuarioLogado)),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

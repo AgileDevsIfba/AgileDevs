b7517933711ab3b00397ecb73c29947fe04f9152
DaianePorcena <daiporcenna@gmail.com>
2025-07-09 19:08:06 -0300

    feat: Add funcionalidade de avaliação de metodos

--- agiledevs/lib/Utils/estado.dart
+++ agiledevs/lib/Utils/estado.dart
@@ -1,9 +1,8 @@
+// ignore: unused_import
 import 'dart:convert';
 
-import 'package:agiledevs/Utils/autenticador.dart';
 import 'package:agiledevs/models/usuario.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 enum Situacao { showMetodos, showDetails, showPractices, showSavedMethods, showModalAvaliacao }
 
@@ -40,7 +39,6 @@ class EstadoApp extends ChangeNotifier {
   }
 
   void showSavedMethods() {
-    print('indo para metodos salvos');
     _situacao = Situacao.showSavedMethods;
     notifyListeners();
   }
@@ -53,7 +51,6 @@ class EstadoApp extends ChangeNotifier {
 
   void onLogin(Usuario? usuario) async {
     _usuario = usuario;
-    await _loadMetodosSalvos();
     notifyListeners();
   }
 
@@ -62,46 +59,6 @@ class EstadoApp extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> salvarMetodo(int idMetodo) async {
-    if (!_metodosSalvos.contains(idMetodo)) {
-      _metodosSalvos.add(idMetodo);
-      await _saveMetodosSalvos();
-      notifyListeners();
-    }
-  }
-
-  Future<void> removerMetodo(int idMetodo) async {
-    _metodosSalvos.remove(idMetodo);
-    await _saveMetodosSalvos();
-    notifyListeners();
-  }
-
-  bool isMetodoSalvo(int idMetodo) {
-    return _metodosSalvos.contains(idMetodo);
-  }
-
-  Future<void> _loadMetodosSalvos() async {
-    if (_usuario == null) return;
-
-    final prefs = await SharedPreferences.getInstance();
-    String? salvos = prefs.getString('metodos_salvos_${_usuario!.email}');
-    if (salvos != null) {
-      _metodosSalvos = List<int>.from(jsonDecode(salvos));
-    } else {
-      _metodosSalvos = [];
-    }
-    notifyListeners();
-  }
-
-  Future<void> _saveMetodosSalvos() async {
-    if (_usuario == null) return;
-
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString(
-      'metodos_salvos_${_usuario!.email}',
-      jsonEncode(_metodosSalvos),
-    );
-  }
 }
 
 late EstadoApp estadoApp;
